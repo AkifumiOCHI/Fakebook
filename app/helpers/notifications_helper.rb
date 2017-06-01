3c079ab1a2d5128e60e5cab03b8badb318765ade
越智 昭文 <akifumi.ochi@gmail.com>
2017-06-01 20:27:01 +0900

    通知の修正

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,5 +1,13 @@
 module NotificationsHelper
   def posted_time(time)
-  time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%m月%d日')
+    time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%Y年%m月%d日 %H:%M:%S')
+  end
+
+  def read_status(read)
+    if read==false
+    "未読"
+    else
+    "既読"
+    end
   end
 end
