878c12ea280b83b9c49220e02ffe10f2caeed6ae
越智 昭文 <akifumi.ochi@gmail.com>
2017-06-01 20:13:27 +0900

    404修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  email = Faker::Internet.email
+  name = "伊藤　博文"
+  password = "password"
+  User.create!(email: email,
+               name: name,
+               password: password,
+               password_confirmation: password,
+               uid: n
+               )
+end
+
+n = 1
+while n <= 100
+  Topic.create(
+    title: "あああ",
+    content: "いいい",
+    user_id: n
+  )
+  n = n + 1
+end
+
+n = 1
+while n <= 100
+  Comment.create(
+    user_id: n,
+    topic_id: 1,
+    content: "コメント"
+  )
+  n = n + 1
+end
