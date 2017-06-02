4599253617b2a6ae12fe7a8abb5f8ed2fb475aaf
越智 昭文 <akifumi.ochi@gmail.com>
2017-06-02 14:12:34 +0900

    change storage to fog from file

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,24 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region:                'ap-northeast-1',
+      path_style:            true,
+  }
+
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'ochi-fakebook-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ochi-fakebook-production'
+    when 'development'
+      config.fog_directory = 'ochi-fakebook-development'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ochi-fakebook-development'
+    when 'test'
+      config.fog_directory = 'ochi-fakebook-test'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ochi-fakebook-test'
+  end
+end
