d8b14897307291a622caefe38fca631a94dd7b5e
越智 昭文 <akifumi.ochi@gmail.com>
2017-06-02 16:54:17 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'Fakebook'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/AkifumiOCHI/Fakebook'
@@ -10,7 +10,7 @@ set :repo_url, 'https://github.com/AkifumiOCHI/Fakebook'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/Fakebook'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
